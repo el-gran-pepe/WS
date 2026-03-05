95054f585d0cb0ab66a72feefdf537ef9ce2e2ff
Juan Jose Pereyra <juan.pereyra@prisma.cc>
2026-03-04 21:12:51 -0300

    AD scripting

--- AD/scripting.ps1
+++ AD/scripting.ps1
@@ -1,2 +1,8 @@
 #Eliminar grupo AD
 Remove-ADGroup -Identity "NombreDelGrupo" -Confirm:$false
+
+#cambiar hostname para luego instalar Rol AD
+Rename-Computer -NewName SRV-VP-DC02 -Restart
+
+#instalar rol AD
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
